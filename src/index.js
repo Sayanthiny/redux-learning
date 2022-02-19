d22d6648c7165e62dca8713b8a60870bf99f1a83
sayanthiny <rmsayanthi@gmail.com>
2022-02-19 19:25:45 +0530

    create action,reducer,store and displays console

--- src/index.js
+++ src/index.js
@@ -3,15 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+
+//selector
+const VADIVELU_COMEDY="VADIVELU_COMEDY"
+const COUNDAMANI_COMEDY="COUNDAMANI_COMEDY"
+
+//Actions
+const VadiveluComedyAction=()=>({type:VADIVELU_COMEDY})
+const CoundamaniComedyAction=()=>({type:COUNDAMANI_COMEDY})
+
+//Reducer
+const comedyReducer=(state,actions)=>{
+  switch (actions.type) {
+    case VADIVELU_COMEDY:
+      return state="kadupethurar my load"
+
+    case COUNDAMANI_COMEDY:
+      return state="arasijalil ethelam satharanam appa"
+  
+    default:
+      return state ="no commedy selected";
+  }
+
+}
+
+const store=createStore(comedyReducer)
+store.subscribe(()=>{console.log(store.getState())})
+store.dispatch(VadiveluComedyAction())
+store.dispatch(CoundamaniComedyAction())
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hi I am sayanthiny</h1>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
